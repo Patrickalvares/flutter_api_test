1b271fee11754e3c9726e98f304cb0d7b98e3af1
Notebook Patrick <patrickalvares@outlook.com>
2023-05-24 10:45:32 -0300

    final

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -41,7 +41,80 @@ class _HomePageState extends State<HomePage> {
           }
           if (store.erro.value.isNotEmpty) {
             return Center(
-              child: Text(store.erro.value),
+              child: Text(
+                store.erro.value,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          if (store.state.value.isEmpty) {
+            return const Center(
+              child: Text(
+                'Nenhum item na lista',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            return ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(height: 32),
+              padding: const EdgeInsets.all(16),
+              itemCount: store.state.value.length,
+              itemBuilder: (_, index) {
+                final item = store.state.value[index];
+                return Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.network(
+                        item.thumbnail,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text(
+                        item.title,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 24),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'R\$ ${item.price}',
+                            style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            item.description,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 18),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
             );
           }
         },
